c234f16c766a122bb9023d024bc1ae8a4ab3f8ca
Maran Sowthri Kalailingam <maransowthri@gmail.com>
2021-11-01 14:11:57 +0530

    Added simple recursion

---
+++ recursion/simple_recursion/sum.py
@@ -0,0 +1,7 @@
+def sum(list_in, index=0):
+    if index == len(list_in):
+        return 0
+    return list_in[index] + recursive_sum(list_in, index + 1)
+
+list_in = [1, 2, 3, 4]
+print(sum(list_in))
