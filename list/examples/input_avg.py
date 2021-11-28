6e307ae4300046aa627980fd4a14ebd116307cc0
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-11-28 13:09:22 +0530

    Finished Python lists module

---
+++ list/examples/input_avg.py
@@ -0,0 +1,20 @@
+# input_list = []
+# while True:
+#     user_input = input('Enter a number: ')
+#     if user_input == 'done':
+#         print(sum(input_list) / len(input_list))
+#         break
+#     input_list.append(float(user_input))
+
+import datetime
+import time
+
+def mystery_func(val=datetime.datetime.now()):
+    print(val)
+
+mystery_func()
+time.sleep(5)
+mystery_func()
+time.sleep(5)
+mystery_func()
+time.sleep(5)
