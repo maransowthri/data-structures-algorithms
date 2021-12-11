fa518a0496415cf397b42a92b208b0768bbde4d8
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-12-11 15:25:30 +0530

    Finished signly linked list module

---
+++ linked_list/singly/challenges/crud.py
@@ -0,0 +1,77 @@
+class Node:
+    def __init__(self, val) -> None:
+        self.val = val
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+    
+    def __iter__(self):
+        temp = self.head
+        while temp:
+            yield temp
+            temp = temp.next
+    
+    def size(self):
+        ll_len = 0
+        temp = self.head
+        
+        while temp:
+            ll_len += 1
+            temp = temp.next
+        
+        return ll_len
+    
+    def append(self, val):
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def insert(self, val, index):
+        node_len = self.size()
+
+        if not 0 <= index <= node_len:
+            raise Exception("Index out of range")
+
+        new_node = Node(val)
+
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+
+            if not self.tail:
+                self.tail = new_node
+        elif index == node_len:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            temp = self.head
+            cur_index = 1
+
+            while index != cur_index:
+                cur_index += 1
+                temp = temp.next
+            
+            new_node.next = temp.next
+            temp.next = new_node
+
+
+
+linked_list = LinkedList()
+input_list = [0, 1, 2, 4]
+
+for val in input_list:
+    linked_list.append(val)
+
+linked_list.insert(3, 3)
+linked_list.insert(5, 5)
+
+for node in linked_list:
+    print(node.val)
--- linked_list/singly/examples/crud.py
+++ linked_list/singly/examples/crud.py
@@ -11,41 +11,37 @@ class LinkedList():
 
     def __iter__(self):
         node = self.head
-        
+
         while node:
             yield node
             node = node.next
-    
-    @staticmethod
-    def len(node):
+
+    def size(self):
         length = 0
-        
+        node = self.head
         while node:
             node = node.next
             length += 1
-        
+
         return length
 
     def append(self, val):
+        new_node = Node(val)
         if not self.head:
-            self.head = Node(val)
-            self.tail = Node(val)
+            self.head = new_node
+            self.tail = new_node
         else:
-            node = self.head
-            while node.next:
-                node = node.next
-            new_node =  Node(val)
-            node.next = new_node
+            self.tail.next = new_node
             self.tail = new_node
 
     def insert(self, val, index):
-        node_len = LinkedList.len(self.head)
-        
+        node_len = self.size()
+
         if not 0 <= index <= node_len:
             raise Exception('Index out of range')
-        
+
         new_node = Node(val)
-        
+
         if index == 0:
             new_node.next = self.head
             self.head = new_node
@@ -58,14 +54,53 @@ class LinkedList():
         else:
             node = self.head
             cur_index = 1
-        
+
             while not cur_index == index:
                 node = node.next
                 cur_index += 1
-        
-            temp = node.next
+
+            new_node.next = node.next
             node.next = new_node
-            new_node.next = temp
+
+    def index(self, val):
+        temp = self.head
+        cur_index = 0
+        while temp:
+            if temp.val == val:
+                return cur_index
+            else:
+                cur_index += 1
+                temp = temp.next
+        raise Exception('Value doesn"t exist')
+
+    def delete(self, index):
+        node_len = self.size()
+
+        if node_len == 0 or not 0 <= index < node_len:
+            raise Exception('Index out of range')
+
+        if index == 0:
+            self.head = self.head.next
+
+            if not self.head:
+                self.tail = None
+        else:
+            temp = self.head
+            cur_index = 0
+
+            while cur_index != index:
+                prev = temp
+                temp = temp.next
+                cur_index += 1
+
+            prev.next = temp.next
+
+            if not temp.next:
+                self.tail = prev
+
+    def clear(self):
+        self.head = None
+        self.tail = None
 
 
 linked_list = LinkedList()
@@ -82,10 +117,26 @@ linked_list.insert(2, 2)
 linked_list.insert(-1, 0)
 linked_list.insert(3, 4)
 
+'''Size'''
+print('Size', linked_list.size())
+
+'''Finding index'''
+print('Index of 0 is', linked_list.index(0))
+# print('Index of 8 is', linked_list.index(8))
+
+'''Deletion'''
+linked_list.delete(1)
+linked_list.delete(0)
+linked_list.delete(2)
+# linked_list.delete(0)
+# linked_list.delete(0)
+
+linked_list.clear()
+
 '''Traverse'''
 for node in linked_list:
     print(node.val)
 
 '''Head & Tail Values'''
-print('First', linked_list.head.val)
-print('Last', linked_list.tail.val)
+print('First', linked_list.head.val if linked_list.head else None)
+print('Last', linked_list.tail.val if linked_list.tail else None)
