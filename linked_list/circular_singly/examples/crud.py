e328526b30822a9c7d0d095f194dbcd5f7b00aac
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-12-14 17:02:23 +0530

    Finished singly circular linked list module

--- linked_list/circular_singly/examples/crud.py
+++ linked_list/circular_singly/examples/crud.py
@@ -8,27 +8,31 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-    
+
     def size(self):
+        if not self.head:
+            return 0
+
         list_len = 1
         temp = self.head
-        
+
         while temp != self.tail:
             list_len += 1
             temp = temp.next
-        
+
         return list_len
-    
+
     def __iter__(self):
         temp = self.head
-        
-        while temp != self.tail:
+
+        while temp:
             yield temp
+
+            if temp == self.tail:
+                break
+
             temp = temp.next
-        
-        if self.tail:
-            yield self.tail
-    
+
     def append(self, val):
         new_node = Node(val)
 
@@ -40,7 +44,7 @@ class LinkedList:
             new_node.next = new_node
             self.head = new_node
             self.tail = new_node
-        
+
     def insert(self, val, index):
         depth = self.size()
 
@@ -48,7 +52,7 @@ class LinkedList:
             raise Exception("Index out of range")
 
         new_node = Node(val)
-        
+
         if index == 0:
             if self.head:
                 new_node.next = self.head
@@ -72,26 +76,42 @@ class LinkedList:
             new_node.next = cur.next
             cur.next = new_node
 
+    def index(self, val):
+        temp = self.head
+        index = 0
+
+        while temp:
+            if temp.val == val:
+                return index
+            if temp == self.tail:
+                raise Exception("Value doesn't exist")
+            temp = temp.next
+            index += 1
+        
+        raise Exception("Value doesn't exist")
+
     def delete(self, index):
         depth = self.size()
 
         if not 0 <= index <= depth:
             raise Exception("Index out of range")
-        
-        if index == 0 and depth == 1:
-            self.head = None
-            self.tail = None
-        elif index == 0:
-            self.head = self.head.next
-            self.tail.next = self.head
+
+        if index == 0:
+            if self.head == self.tail:
+                self.head.next = None
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.next
+                self.tail.next = self.head
         else:
             cur_index = 1
             cur = self.head
 
             while index != cur_index:
                 cur = cur.next
-                cur_index += 1        
-            
+                cur_index += 1
+
             if cur_index == depth - 1:
                 cur.next = self.head
                 self.tail = cur
@@ -99,6 +119,7 @@ class LinkedList:
                 cur.next = cur.next.next
 
     def clear(self):
+        self.tail.next = None
         self.head = None
         self.tail = None
 
@@ -119,7 +140,15 @@ linked_list.insert(1, 1)
 '''Deletion'''
 linked_list.delete(0)
 linked_list.delete(1)
-linked_list.delete(2)
+# linked_list.delete(2)
+
+'''Find index'''
+print('Index of 1 is', linked_list.index(1))
+print('Index of 3 is', linked_list.index(3))
+print('Index of 4 is', linked_list.index(4))
+
+'''Clear Everything'''
+linked_list.clear()
 
 '''Traversal'''
 for node in linked_list:
